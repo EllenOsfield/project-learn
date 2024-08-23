90d04881643b98c37b7703fbd97ae09caac6e120
Ellen Osfield <Ellen.Osfield@gmail.com>
2024-08-23 14:15:59 +0100

    simplyfying row number code

--- nanogramexample/thirdattempt.cpp
+++ nanogramexample/thirdattempt.cpp
@@ -132,44 +132,61 @@ public:
 
 };
 
+std::vector<int> computeRowCounts(Grid& grid, int row)
+{
+    std::vector<int> nums;
+    int mus = 0;
+    int ums = 0;
+    int msu = 0;
+    for(uint32_t column=0; column<grid.width(); ++column)
+    {
+        if (grid.at(column,row) == 1)
+        {
+            mus += 1,
+            ums += 1;
+        }
+
+        if (ums == 1)
+            msu += 1;
+        else
+            if (msu != 0)
+            {
+                nums.push_back(msu);
+                //std::cout<<"num "<<num<<std::endl;
+                msu = 0;
+            }
+            else
+                msu = 0;
+
+        // std::cout<<"column "<<ctimes<<" sum = "<<ums<<" and current run  = "<<msu<<std::endl;
+        ums = 0;
+    }
+
+    if (msu != 0)
+    {
+        nums.push_back(msu);
+    }
+
+    return nums;
+}
+
 int counter(Grid& grid)
 {
    int sum = 0 ;
-   int mus = 0;
-   int ums = 0;
-   int msu = 0;
-   int rtimes = 0;
-   int ctimes = 0;
-   int num = {};
 
     for(uint32_t row=0; row<grid.height(); ++row)
+    {
+        auto nums = computeRowCounts(grid, row);
+
+        std::stringstream str;
+        for(int n : nums)
         {
-            for(uint32_t column=0; column<grid.width(); ++column)
-            {
-                if (grid.at(column,row) == 1)
-                    sum += 1,
-                    mus += 1,
-                    ums += 1;
-                ctimes +=1;
-                if (ums == 1)
-                    msu += 1;
-                else
-                    if (msu != 0)
-                        num = msu,
-                        std::cout<<"num "<<num<<std::endl,
-                        msu = 0;
-                    else
-                        msu = 0;
-
-               // std::cout<<"column "<<ctimes<<" sum = "<<ums<<" and current run  = "<<msu<<std::endl;
-                ums = 0;
-            }
-            msu = 0;
-            rtimes += 1;
-            std::cout<<"row "<<rtimes<<" sum = "<<mus<<std::endl;
-            mus = 0;
-
+            str<<n<<" ";
         }
+
+        std::cout<<"row "<<row<<" [ "<< str.str()<<"]"<<std::endl;
+
+    }
     std::cout<<"total sum ="<<sum<<std::endl;
 
     return sum;
